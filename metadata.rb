5660d0792b388803df7e594357c5aca7dfff5006
Christian Trabold <ct@192-168-0-28.dkd.lan>
2011-10-20 08:11:11 +0200

    Change version from 0.2 to 0.0.4 to have consistent version schema. Sorry for confusion

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@christian-trabold.de"
 license          "MIT"
 description      "Installs/Configures Pirum - A simple PEAR Channel Server Manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2"
+version          "0.0.4"
 
 %w[php].each do |cb|
   depends cb
