e3bab8771f8410e0953caad6c38d31c47dce4e03
Christian Trabold <ct@192-168-0-28.dkd.lan>
2011-10-20 08:10:07 +0200

    Bump version from 0.1 to 0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@christian-trabold.de"
 license          "MIT"
 description      "Installs/Configures Pirum - A simple PEAR Channel Server Manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1"
+version          "0.2"
 
 %w[php].each do |cb|
   depends cb
