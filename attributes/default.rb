af97a5c31d3d5cef42d9328e15d6b6c472058c46
Christian Trabold <ct@192-168-0-28.dkd.lan>
2011-10-20 08:09:42 +0200

    Add header comments

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,23 @@
+#
+# Cookbook Name:: pirum
+# Attributes:: pirum
+#
+# Copyright 2011, dkd Internet Service GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Set general properties
 set_unless[:pirum][:port]        = 8080
 set_unless[:pirum][:hostname]    = "pear.example.com"
 set_unless[:pirum][:name]        = "Your PEAR channel"
